897449a5da3980696e2c20349dd3b90d5f21e794
AhmedMustafa48 <ahmedcodes48@gmail.com>
2025-10-05 14:15:22 +0500

    Add profile and author routes; remove alert from script.js

--- tutorial-six/index.js
+++ tutorial-six/index.js
@@ -12,6 +12,16 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+app.get("/profile/:username", function (req, res) {
+  req.params.username;
+  res.send(`hello ${req.params.username}`);
+});
+
+
+app.get("/author/:username/:age", function (req, res) {
+  res.send(`hello ${req.params.username}, you are ${req.params.age} years old`);
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
