afd9546d362ddc78c0484cdcb15d2c40c01196af
AhmedMustafa48 <ahmedcodes48@gmail.com>
2025-10-08 23:38:41 +0500

    create api

--- tutorial-eight/app.js
+++ tutorial-eight/app.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
+app.get("/create", async (req, res) => {
+  let createdUser = await userModel.create({
+    name: "faisal",
+    email: "xyz@gmail.com",
+    username: "jan",
+  });
+
+  res.send(createdUser);
+});
+
 
 
 app.listen(3000);
